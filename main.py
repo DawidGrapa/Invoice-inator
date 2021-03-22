a0dff614f881e6b54dd96e97621daee1f49235fe
Dawid <dgrapa2000@gmail.com>
2021-03-22 16:57:45 +0100

    Cos sie zaczyna

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("DUPA")
-print("uWu")
+from tkinter import *
+from Invoice import *
+
+def create():
+    label = Label(root, text="CIEKAWE")
+    label.pack()
+
+root = Tk()
+
+createInvoice = Button(root, text="Create Invoice",command=create)
+createInvoice.pack()
+root.mainloop()
