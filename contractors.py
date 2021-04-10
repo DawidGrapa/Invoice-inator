128cbebc750d3259f54dea4f66fecf1355fe6579
Lucia <szpak.julia14@gmail.com>
2021-04-10 10:58:08 +0200

    „”

--- contractors.py
+++ contractors.py
@@ -2,47 +2,48 @@ import tkinter as tk
 from tkinter import *
 from tkinter.ttk import *
 
-def save(contractor):
-    return
 
-def addContractor(app):
+def print_collected_data(data):
+    for key, el in data.items():
+        print(key, ': ', el.get())
+
+
+def add_contractor_window(app):
     newWindow = Toplevel(app)
     newWindow.title("Add new contractor")
     newWindow.minsize(500, 260)
 
+    # Dictionary for storing collected data
     contractorData = dict()
 
-    #Name
-    name = tk.Label(newWindow, text="Company Name:", height=2,padx=10)
+    # Labels and Entries
+    # Name
+    name = tk.Label(newWindow, text="Company Name:", height=2, padx=10)
     name.grid(row=1, column=1)
-
     nameInput = tk.Entry(newWindow, width=50, bd=3)
     nameInput.grid(row=1, column=2)
 
     contractorData['name'] = nameInput
 
-    #Street
-    street = tk.Label(newWindow,text="Street:",height = 2,padx=10)
-    street.grid(row=2,column=1)
-
+    # Street
+    street = tk.Label(newWindow, text="Street:", height=2, padx=10)
+    street.grid(row=2, column=1)
     streetInput = tk.Entry(newWindow, width=50, bd=3)
     streetInput.grid(row=2, column=2)
 
     contractorData['street'] = streetInput
 
-    #ZIP-CODE
-    zip_code = tk.Label(newWindow,text="Zip-Code:",height = 2,padx=10)
-    zip_code.grid(row=3,column=1)
-
+    # ZIP-CODE
+    zipCode = tk.Label(newWindow, text="Zip-Code:", height=2, padx=10)
+    zipCode.grid(row=3, column=1)
     zipInput = tk.Entry(newWindow, width=50, bd=3)
     zipInput.grid(row=3, column=2)
 
     contractorData['zip'] = zipInput
 
-    #City
+    # City
     city = tk.Label(newWindow, text="City:", height=2, padx=10)
     city.grid(row=4, column=1)
-
     cityInput = tk.Entry(newWindow, width=50, bd=3)
     cityInput.grid(row=4, column=2)
 
@@ -51,7 +52,6 @@ def addContractor(app):
     # NIP
     nip = tk.Label(newWindow, text="NIP:", height=2, padx=10)
     nip.grid(row=5, column=1)
-
     nipInput = tk.Entry(newWindow, width=50, bd=3)
     nipInput.grid(row=5, column=2)
 
@@ -60,40 +60,40 @@ def addContractor(app):
     # Description
     desc = tk.Label(newWindow, text="Description:", height=2, padx=10)
     desc.grid(row=6, column=1)
-
     descInput = tk.Entry(newWindow, width=50, bd=3)
     descInput.grid(row=6, column=2)
 
     contractorData['desc'] = descInput
 
-    #Submit
-    submitLabel = tk.Button(newWindow,text="Submit",command = lambda: save(contractorData))
-    submitLabel.grid(row=7,column=2)
-
-
+    # Submit
+    submitLabel = tk.Button(newWindow, text="Submit", command=lambda: print_collected_data(contractorData))
+    submitLabel.grid(row=7, column=2)
 
 
 def open_contractors_window(app):
+    # Creating new window
     newWindow = Toplevel(app)
     newWindow.title("Contractors")
     newWindow.geometry('700x500')
+    newWindow.minsize(600, 100)
     newWindow['bg'] = '#f8deb4'
-    newWindow.minsize(600,100)
-    # Create Panedwindow
+
+    # Creating PanedWindow - for splitting frames in ratio
     panedwindow = Panedwindow(newWindow, orient=HORIZONTAL)
     panedwindow.pack(fill=BOTH, expand=True)
-    # Create Frams
+
+    # Creating Frames
     fram1 = tk.Frame(panedwindow, width=100, height=300, relief=SUNKEN)
-    fram2 = tk.Frame(panedwindow, width=400, height=400, relief=SUNKEN,bg = '#94dbd6')
+    fram2 = tk.Frame(panedwindow, width=400, height=400, relief=SUNKEN, bg='#94dbd6')
     panedwindow.add(fram1, weight=1)
     panedwindow.add(fram2, weight=4)
 
-    L1 = tk.Label(fram1)
-    L1.pack()
-    L2 = tk.Label(fram1)
-    L2.pack()
+    addContLabel = tk.Label(fram1)
+    addContLabel.pack()
+    deleteContLabel = tk.Label(fram1)
+    deleteContLabel.pack()
 
-    addContrahentButton = tk.Button(L1,text="Add new contractor",height=2,width = 20,command = lambda: addContractor(newWindow))
-    addContrahentButton.pack(fill=BOTH,side=LEFT, expand=True)
-    deleteContractorButton = tk.Button(L2,text="Delete contractor",height=2,width=20)
-    deleteContractorButton.pack(fill=BOTH,side=LEFT, expand=True)
+    addContrahentButton = tk.Button(addContLabel, text="Add new contractor", height=2, width=20, command=lambda: add_contractor_window(newWindow))
+    addContrahentButton.pack(fill=BOTH, side=LEFT, expand=True)
+    deleteContractorButton = tk.Button(deleteContLabel, text="Delete contractor", height=2, width=20)
+    deleteContractorButton.pack(fill=BOTH, side=LEFT, expand=True)
--- main.py
+++ main.py
@@ -2,32 +2,29 @@ from tkinter import *
 from tkinter.ttk import *
 from contractors import *
 
-#Create window object
+# Creating window app
 app = Tk()
-app['bg']='#f8deb4'
 app.title("Apka")
+#app.state("zoomed")
+app['bg'] = '#f8deb4'
+# Change app icon
+photo = PhotoImage(file='soy.png')
+app.iconphoto(True, photo)
 
-app.state("zoomed")
-
-#Change app icon
-photo = PhotoImage(file = 'flow.png')
-app.iconphoto(False, photo)
-
-#Create Menu
+# Creating menu
 my_menu = Menu(app)
-app.config(menu = my_menu)
+app.config(menu=my_menu)
 
-file_menu= Menu(my_menu,tearoff = False)
-file_menu.add_command(label="Create invoice",font= 'Helvetica 10 bold')
+file_menu = Menu(my_menu, tearoff=False)
+file_menu.add_command(label="Create invoice", font='Helvetica 10 bold')
 file_menu.add_separator()
-file_menu.add_command(label="List of contractors",command = lambda: open_contractors_window(app))
-settings_menu = Menu(my_menu,tearoff= False)
+file_menu.add_command(label="List of contractors", command=lambda: open_contractors_window(app))
+
+settings_menu = Menu(my_menu, tearoff=False)
 
 my_menu.add_cascade(label="MENU", menu=file_menu)
 my_menu.add_separator()
-my_menu.add_cascade(label="Settings",menu=settings_menu)
+my_menu.add_cascade(label="Settings", menu=settings_menu)
 
-
-
-#Start of the program
+# Start of the program
 app.mainloop()
